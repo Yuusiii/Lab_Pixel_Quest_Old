6efb965e7677784e0b953208a6bfd72bf6be5aed
Yuusiii <dkagbala4@gmail.com>
2025-07-10 12:12:07 -0400

    More Coding
    lebron james butt

--- Assets/Geo_Quest/Scripts/GeoControl.cs
+++ Assets/Geo_Quest/Scripts/GeoControl.cs
@@ -1,34 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class GeoControl : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    int varTwo = 3;
+    public int speed = 3;
     // Start is called before the first frame update
     void Start()
     {
-
+       rb= GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += new Vector3(1, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 1, 0);
 
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+
     }
 }
+
+
