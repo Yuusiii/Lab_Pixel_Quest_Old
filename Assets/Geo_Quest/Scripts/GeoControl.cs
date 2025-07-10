f5e59100affcc084885ce44081013ccf698a3b23
Yuusiii <dkagbala4@gmail.com>
2025-07-10 10:49:46 -0400

    movment
    ts so tuff bro

--- Assets/Geo_Quest/Scripts/GeoControl.cs
+++ Assets/Geo_Quest/Scripts/GeoControl.cs
@@ -7,12 +7,28 @@ public class GeoControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0.005f, 0,0);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+
+        }
     }
 }
